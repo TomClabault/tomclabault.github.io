10484e1196d3346609f580f8ceb911a4e03cf770
Tom Clabault <email>
2024-08-07 19:01:37 +0200

    ImageBox Responsive Setup

---
+++ assets/blogs-assets/Per-Pixel-Adaptive-Sampling/ImageBox/data.js
@@ -0,0 +1,34 @@
+var data = {
+    "imageBoxes": [    
+        {
+            "title": "Real-Time (Equal Time, â‰ˆ35 ms)",
+            "elements": [
+                {
+                    "title": "Real-Time Rendering",
+                    "elements": [
+                        {
+                            "image": "/assets/blogs-assets/Per-Pixel-Adaptive-Sampling/ImageBox/2x.jpg",
+                            "title": "Path Tracing (direct+indirect)"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/Per-Pixel-Adaptive-Sampling/ImageBox/1x.jpg",
+                            "title": "Path Tracing + ReSTIR DI"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/Per-Pixel-Adaptive-Sampling/ImageBox/1x.jpg",
+                            "title": "ReSTIR PT (Reconnection) + ReSTIR DI"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/Per-Pixel-Adaptive-Sampling/ImageBox/2x.jpg",
+                            "title": "ReSTIR PT (Hybrid) + ReSTIR DI"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/Per-Pixel-Adaptive-Sampling/ImageBox/3x.jpg",
+                            "title": "Reference (direct+indirect)"
+                        },                        
+                    ]
+                }
+            ]
+        }, 
+    ]
+}
