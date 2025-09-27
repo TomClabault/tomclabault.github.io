74b2a14dee3a8358d9c54f93595eec58d5735ce6
Tom Clabault <email>
2025-09-27 09:50:04 +0200

    ReGIR blog post ++

---
+++ assets/blogs-assets/ReGIR-Many-Lights/ImageBox/dataRISNoVisibility.js
@@ -0,0 +1,34 @@
+var dataRISNoVisibility = {
+    "imageBoxes": [    
+        {
+            "title": "RIS 1SPP, pHat without visibiilty",
+            "elements": [
+                {
+                    "title": "",
+                    "elements": [
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/ris-no-visibility/10.png",
+                            "title": "M = 10"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/ris-no-visibility/100.png",
+                            "title": "M = 100"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/ris-no-visibility/1000.png",
+                            "title": "M = 1000"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/ris-no-visibility/10000.png",
+                            "title": "M = 10000"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/ris-no-visibility/40000.png",
+                            "title": "M = 40000"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
