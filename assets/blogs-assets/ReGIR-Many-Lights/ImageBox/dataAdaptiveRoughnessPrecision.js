a2939961783a39450731f5b67a718434e38d4074
Tom Clabault <email>
2025-10-03 16:50:05 +0200

    ReGIR blog post++. BSDF resampling done

---
+++ assets/blogs-assets/ReGIR-Many-Lights/ImageBox/dataAdaptiveRoughnessPrecision.js
@@ -0,0 +1,59 @@
+var dataAdaptiveRoughnessPrecision = {
+    "imageBoxes": [    
+        {
+            "title": "Quality with BSDF in the target function + adaptive grid precision (AGP). L=(varrying), R=64, S=1",
+            "elements": [
+				{
+                    "title": "The white room α=0.1",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/quality-0.1-no-precision-twr.png",
+                            "title": "Quality no AGP"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/quality-0.1-precision-twr-l32.png",
+                            "title": "Quality AGP L=32"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/quality-0.1-precision-twr-l128.png",
+                            "title": "Quality AGP L=128"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/grid-0.1-no-precision-twr.png",
+                            "title": "Grid no AGP"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/grid-0.1-precision-twr.png",
+                            "title": "Grid AGP"
+                        }
+                    ]
+                },
+				{
+                    "title": "The white room α=0.2",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/quality-0.2-no-precision-twr.png",
+                            "title": "Quality no AGP"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/quality-0.2-precision-twr-l32.png",
+                            "title": "Quality AGP L=32"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/quality-0.2-precision-twr-l128.png",
+                            "title": "Quality AGP L=128"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/grid-0.2-no-precision-twr.png",
+                            "title": "Grid no AGP"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-adaptive-roughness-precision/grid-0.2-precision-twr.png",
+                            "title": "Grid AGP"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
---
+++ assets/blogs-assets/ReGIR-Many-Lights/ImageBox/dataRegirBSDFTargetFunction.js
@@ -0,0 +1,123 @@
+var dataRegirBSDFTargetFunction = {
+    "imageBoxes": [    
+        {
+            "title": "Quality with BSDF in the target function. L=32, R=64, S=1",
+            "elements": [
+				{
+                    "title": "The white room α=0",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/ref-twr-0.png",
+                            "title": "Reference"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/no-bsdf-twr-0.png",
+                            "title": "no BSDF term"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/bsdf-twr-0.png",
+                            "title": "+BSDF term"
+                        }
+                    ]
+                },
+				
+				{
+                    "title": "The white room α=0.1",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/ref-twr-0.1.png",
+                            "title": "Reference"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/no-bsdf-twr-0.1.png",
+                            "title": "no BSDF term"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/bsdf-twr-0.1.png",
+                            "title": "+BSDF term"
+                        }
+                    ]
+                },
+				
+				{
+                    "title": "The white room α=0.2",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/ref-twr-0.2.png",
+                            "title": "Reference"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/no-bsdf-twr-0.2.png",
+                            "title": "no BSDF term"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/bsdf-twr-0.2.png",
+                            "title": "+BSDF term"
+                        }
+                    ]
+                },
+				
+				{
+                    "title": "The white room α=0.4",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/ref-twr-0.4.png",
+                            "title": "Reference"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/no-bsdf-twr-0.4.png",
+                            "title": "no BSDF term"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/bsdf-twr-0.4.png",
+                            "title": "+BSDF term"
+                        }
+                    ]
+                },
+				
+				
+				
+				
+				
+				
+                {
+                    "title": "Bistro",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/ref-bistro.png",
+                            "title": "Reference"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/no-bsdf-bistro.png",
+                            "title": "no BSDF term"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/bsdf-bistro.png",
+                            "title": "+BSDF term"
+                        }
+                    ]
+                },
+				
+				
+				
+				{
+                    "title": "BZD measure-seven",
+                    "elements": [
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/ref-bzd.png",
+                            "title": "Reference"
+                        },
+						{
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/no-bsdf-bzd.png",
+                            "title": "no BSDF term"
+                        },
+                        {
+                            "image": "/assets/blogs-assets/ReGIR-Many-Lights/ImageBox/imgs/regir-bsdf-target-function/bsdf-bzd.png",
+                            "title": "+BSDF term"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
